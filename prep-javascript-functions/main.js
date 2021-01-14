72644f17a13f1dfc258aba329fabff9c20c89073
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-01-14 14:44:28 -0800

    completed addTwoNumbers and convertHoursToMinutes exercises

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+var addTwoNumbersResult = addTwoNumbers(4,4);
+console.log('addTwoNumbers Exercise: ' + addTwoNumbersResult);
+
+function convertHoursToMinutes(number) {
+  return number * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(3);
+console.log('convertHoursToMinutes Exercise; ' + convertHoursToMinutesResult);
