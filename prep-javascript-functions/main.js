698e3189baa0efe0c3a45f613b713e286b4cbb61
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-01-14 15:14:58 -0800

    exercises multiplyAndDivideBy5 and subtractTwoNumbers

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -25,3 +25,22 @@ function returnDataType(data) {
 
 var returnDataTypeResult = returnDataType(100);
 console.log('returnDataType Exercise: ' + returnDataTypeResult);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+var addAndMultiplyBy5Result = addAndMultiplyBy5(5, 5);
+console.log('addAndMultiplyBy5 Exercise: ' + addAndMultiplyBy5Result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(10, 10);
+console.log('multiplyAndDivideBy5 Excercise: ' + multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+var  subtractTwoNumbersResult = subtractTwoNumbers(10, 10);
+console.log('subtractTwoNumbers Excercise: ' + subtractTwoNumbersResult);
