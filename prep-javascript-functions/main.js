a422ebfbe735677e7af8aef104b7de19a2749fc9
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-01-14 14:59:04 -0800

    completed returnDataType exercise

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -10,4 +10,18 @@ function convertHoursToMinutes(number) {
 }
 
 var convertHoursToMinutesResult = convertHoursToMinutes(3);
-console.log('convertHoursToMinutes Exercise; ' + convertHoursToMinutesResult);
+console.log('convertHoursToMinutes Exercise: ' + convertHoursToMinutesResult);
+
+function personalizeGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+var personalizeGreetingResult = personalizeGreeting('Janelle');
+console.log('personalizeGreeting Exercise: ' + personalizeGreetingResult);
+
+function returnDataType(data) {
+  return typeof(data);
+}
+
+var returnDataTypeResult = returnDataType(100);
+console.log('returnDataType Exercise: ' + returnDataTypeResult);
